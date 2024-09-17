2287a68b661bf60a78248b3a44d7f01c22ffdde5
Zaid316 <zaidfarookhi@yahoo.com>
2024-09-17 17:05:22 +0530

    zipped

--- booksystem2/booksystem/src/main/java/com/code/booksystem/Exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.code.booksystem.Exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);  // Pass the message to the parent class (RuntimeException)
-    }
-}
--- booksystem2/booksystem/src/main/java/com/code/booksystem/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.code.booksystem.services;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-	 UserDetailsService userDetailsService();
-}
